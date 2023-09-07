1ce9e1a4350bdee0800e07e549ddf8df707bfc1a
t0nn1x <antoonhrobust@gmail.com>
2023-09-07 20:23:34 +0200

    Created JavaDoc documentation for all methods of the service module

--- src/main/java/com/testapi/accounting/service/AccountingEntryService.java
+++ src/main/java/com/testapi/accounting/service/AccountingEntryService.java
@@ -16,18 +16,40 @@ public class AccountingEntryService {
         this.accountingEntryRepository = accountingEntryRepository;
     }
 
+    /**
+     * Finds an accounting entry by ID.
+     *
+     * @param id the ID of the accounting entry
+     * @return an Optional<AccountingEntry> which contains the entry if found
+     */
     public Optional<AccountingEntry> findById(Long id) {
         return accountingEntryRepository.findById(id);
     }
 
+    /**
+     * Retrieves all accounting entries from the database.
+     *
+     * @return a List of accounting entries
+     */
     public List<AccountingEntry> findAll(){
         return accountingEntryRepository.findAll();
     }
 
+    /**
+     * Saves a new accounting entry or updates an existing one.
+     *
+     * @param accountingEntry the entry to be saved
+     * @return the saved entry
+     */
     public AccountingEntry save(AccountingEntry accountingEntry){
         return accountingEntryRepository.save(accountingEntry);
     }
 
+    /**
+     * Deletes an accounting entry by ID.
+     *
+     * @param id the ID of the entry to be deleted
+     */
     public void deleteById(Long id){
         accountingEntryRepository.deleteById(id);
     }
--- src/main/java/com/testapi/accounting/service/UserService.java
+++ src/main/java/com/testapi/accounting/service/UserService.java
@@ -18,18 +18,41 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Finds a user by ID.
+     *
+     * @param id the ID of the user
+     * @return an Optional<User> which contains the user if found
+     */
     public Optional<User> findById(Long id){
         return userRepository.findById(id);
     }
 
+    /**
+     * Retrieves all users from the database.
+     *
+     * @return a List of users
+     */
     public List<User> findAll(){
         return userRepository.findAll();
     }
+
+    /**
+     * Saves a new user or updates an existing one.
+     *
+     * @param user the user to be saved
+     * @return the saved user
+     */
 
     public User save(User user){
         return userRepository.save(user);
     }
 
+    /**
+     * Deletes a user by ID.
+     *
+     * @param id the ID of the user to be deleted
+     */
     public void deleteById(Long id){
         userRepository.deleteById(id);
     }
