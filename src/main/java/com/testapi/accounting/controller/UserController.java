fa2df8a28f17e2b0c3abd8fd63114ab9e5b8bbc2
t0nn1x <antoonhrobust@gmail.com>
2023-09-09 12:44:17 +0200

    Refactored user controller

--- src/main/java/com/testapi/accounting/controller/UserController.java
+++ src/main/java/com/testapi/accounting/controller/UserController.java
@@ -2,34 +2,19 @@ package com.testapi.accounting.controller;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.testapi.accounting.constant.UserRole;
 import com.testapi.accounting.entity.User;
-import com.testapi.accounting.service.FileStorageService;
 import com.testapi.accounting.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    @Autowired
-    private FileStorageService fileStorageService;
-
     private final UserService userService;
 
     @Autowired
@@ -37,13 +22,11 @@ public class UserController {
         this.userService = userService;
     }
 
-    // Get all users
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.findAll());
     }
 
-    // Get user by id
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
         return userService.findById(id)
@@ -51,104 +34,23 @@ public class UserController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    // Create a new user
     @PostMapping
-    public ResponseEntity<?> createUser( // ResponseEntity<?> is used to return any type of response
-            @RequestParam(value = "file", required = false) MultipartFile file,
-            @RequestParam("email") String email,
-            @RequestParam("password") String password,
-            @RequestParam(value = "role", required = false, defaultValue = "NORMAL") UserRole role,
-            @RequestParam(value = "loggedInAt", required = false) LocalDateTime loggedInAt,
-            @RequestParam(value = "updatedAt", required = false) LocalDateTime updatedAt) throws IOException {
-        User user = new User();
-        user.setEmail(email);
-        user.setPassword(password);
-        user.setRole(role); // This will set the role to NORMAL if not provided in the request
-
-        if (email == null || email.isEmpty() || email.length() > 255 || !email.contains("@")) {
-            return ResponseEntity.badRequest().body("Invalid email format.");
-        }
-
-        if (password == null || password.length() < 8) {
-            return ResponseEntity.badRequest().body("Password must be at least 8 characters.");
-        }
-
-        if (loggedInAt != null) {
-            user.setLoggedInAt(loggedInAt);
-        }
-        if (updatedAt != null) {
-            user.setUpdatedAt(updatedAt);
-        }
-
-        if (file != null && !file.isEmpty()) {
-            String fileName = fileStorageService.storeFile(file);
-            user.setPhotoPath(fileName);
-        }
-
-        return ResponseEntity.ok(userService.save(user));
+    public ResponseEntity<?> createUser(User user, @RequestParam(value = "file", required = false) MultipartFile file) throws IOException {
+        return ResponseEntity.ok(userService.createUser(user, file));
     }
 
-    // Update user
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateUser(
-            @PathVariable Long id,
-            @RequestParam(value = "file", required = false) MultipartFile file,
-            @RequestParam(value = "email", required = false) String email,
-            @RequestParam(value = "password", required = false) String password,
-            @RequestParam(value = "role", required = false) UserRole role,
-            @RequestParam(value = "loggedInAt", required = false) LocalDateTime loggedInAt,
-            @RequestParam(value = "updatedAt", required = false) LocalDateTime updatedAt,
-            Principal principal) throws IOException {
-        return userService.findById(id).map(user -> {
-            if (!principal.getName().equals("ADMIN") && !principal.getName().equals(email)) {
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied");
-            }
-
-            if (email != null) {
-                user.setEmail(email);
-            }
-            if (password != null) {
-                user.setPassword(password);
-            }
-            if (role != null) {
-                user.setRole(role);
-            }
-            if (loggedInAt != null) {
-                user.setLoggedInAt(loggedInAt);
-            }
-            if (updatedAt != null) {
-                user.setUpdatedAt(updatedAt);
-            }
-
-            if (file != null && !file.isEmpty()) {
-                String fileName = fileStorageService.storeFile(file);
-                user.setPhotoPath(fileName);
-            }
-
-            if (email != null && (email.isEmpty() || email.length() > 255 || !email.contains("@"))) {
-                return ResponseEntity.badRequest().body("Invalid email format.");
-            }
-
-            if (password != null && password.length() < 8) {
-                return ResponseEntity.badRequest().body("Password must be at least 8 characters.");
-            }
-
-            return ResponseEntity.ok(userService.save(user));
-        }).orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<?> updateUser(@PathVariable Long id, User updatedUser,
+            @RequestParam(value = "file", required = false) MultipartFile file) throws IOException {
+        try {
+            return ResponseEntity.ok(userService.updateUser(id, updatedUser, file));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
-    // Delete user by id
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id, Principal principal) {
-        User userToDelete = userService.findById(id).orElse(null);
-        if (userToDelete == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        if (!principal.getName().equals("ADMIN") && !principal.getName().equals(userToDelete.getEmail())) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
-        }
-
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/testapi/accounting/service/UserService.java
+++ src/main/java/com/testapi/accounting/service/UserService.java
@@ -1,18 +1,23 @@
 package com.testapi.accounting.service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.testapi.accounting.entity.User;
 import com.testapi.accounting.repository.UserRepository;
+import com.testapi.accounting.constant.UserRole;
+
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final FileStorageService fileStorageService; 
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -20,49 +25,76 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.fileStorageService = new FileStorageService();
     }
 
-    /**
-     * Finds a user by ID.
-     *
-     * @param id the ID of the user
-     * @return an Optional<User> which contains the user if found
-     */
     public Optional<User> findById(Long id) {
         return userRepository.findById(id);
     }
 
-    /**
-     * Retrieves all users from the database.
-     *
-     * @return a List of users
-     */
     public List<User> findAll() {
         return userRepository.findAll();
     }
-
-    /**
-     * Saves a new user or updates an existing one.
-     *
-     * @param user the user to be saved
-     * @return the saved user
-     */
 
     public User save(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
-    /**
-     * Deletes a user by ID.
-     *
-     * @param id the ID of the user to be deleted
-     */
     public void deleteById(Long id) {
         userRepository.deleteById(id);
     }
 
     public User findByEmail(String email) {
         return (User) userRepository.findByEmail(email).orElse(null);
+    }
+
+    public User createUser(User user, MultipartFile file) throws IOException {
+        if (user.getEmail() == null || user.getEmail().isEmpty() || user.getEmail().length() > 255 || !user.getEmail().contains("@")) {
+            throw new IllegalArgumentException("Invalid email format.");
+        }
+
+        if (user.getPassword() == null || user.getPassword().length() < 8) {
+            throw new IllegalArgumentException("Password must be at least 8 characters.");
+        }
+
+        if (file != null && !file.isEmpty()) {
+            String fileName = fileStorageService.storeFile(file);
+            user.setPhotoPath(fileName);
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User updatedUser, MultipartFile file) throws IOException {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        if (updatedUser.getEmail() != null) {
+            if (updatedUser.getEmail().isEmpty() || updatedUser.getEmail().length() > 255
+                    || !updatedUser.getEmail().contains("@")) {
+                throw new IllegalArgumentException("Invalid email format.");
+            }
+            user.setEmail(updatedUser.getEmail());
+        }
+
+        if (updatedUser.getPassword() != null && updatedUser.getPassword().length() < 8) {
+            throw new IllegalArgumentException("Password must be at least 8 characters.");
+        }
+
+        if (file != null && !file.isEmpty()) {
+            String fileName = fileStorageService.storeFile(file);
+            user.setPhotoPath(fileName);
+        }
+
+        if (updatedUser.getPassword() != null) {
+            user.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+        }
+
+        if (updatedUser.getRole() != null) {
+            user.setRole(updatedUser.getRole());
+        }
+
+        return userRepository.save(user);
     }
 }
