ef80a81b542c8065d618ce502b7d92d39a3ce41d
t0nn1x <antoonhrobust@gmail.com>
2023-09-09 13:01:59 +0200

    Refactored entry controller

--- src/main/java/com/testapi/accounting/controller/AccountingEntryController.java
+++ src/main/java/com/testapi/accounting/controller/AccountingEntryController.java
@@ -7,19 +7,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.testapi.accounting.entity.AccountingEntry;
 import com.testapi.accounting.entity.User;
-import com.testapi.accounting.repository.AccountingEntryService;
+import com.testapi.accounting.service.AccountingEntryService;
 import com.testapi.accounting.service.UserService;
 
 @RestController
@@ -35,19 +27,16 @@ public class AccountingEntryController {
         this.accountingEntryService = accountingEntryService;
     }
 
-    // Get all entries
     @GetMapping
     public ResponseEntity<List<AccountingEntry>> getAllEntries(Principal principal) {
         if (principal.getName().equals("ADMIN")) {
             return ResponseEntity.ok(accountingEntryService.findAll());
         } else {
-            // Fetch entries only for the logged-in NORMAL user
             User user = userService.findByEmail(principal.getName());
             return ResponseEntity.ok(accountingEntryService.findByUser(user));
         }
     }
 
-    // Get entry by id
     @GetMapping("/{id}")
     public ResponseEntity<AccountingEntry> getEntry(@PathVariable Long id) {
         return accountingEntryService.findById(id)
@@ -55,26 +44,17 @@ public class AccountingEntryController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    // Create a new entry
     @PostMapping
     public ResponseEntity<AccountingEntry> createEntry(@Valid @RequestBody AccountingEntry entry) {
         return ResponseEntity.ok(accountingEntryService.save(entry));
     }
 
-    // Update entry
     @PutMapping("/{id}")
     public ResponseEntity<AccountingEntry> updateEntry(@PathVariable Long id,
             @Valid @RequestBody AccountingEntry updatedEntry) {
-        return accountingEntryService.findById(id).map(entry -> {
-            entry.setEntryText(updatedEntry.getEntryText());
-            entry.setRecordType(updatedEntry.getRecordType());
-            entry.setAmount(updatedEntry.getAmount());
-
-            return ResponseEntity.ok(accountingEntryService.save(entry));
-        }).orElse(ResponseEntity.notFound().build());
+        return accountingEntryService.updateEntry(id, updatedEntry);
     }
 
-    // Delete entry by id
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEntity(@PathVariable Long id) {
         accountingEntryService.deleteById(id);
--- src/main/java/com/testapi/accounting/service/AccountingEntryService.java
+++ src/main/java/com/testapi/accounting/service/AccountingEntryService.java
@@ -1,13 +1,15 @@
-package com.testapi.accounting.repository;
+package com.testapi.accounting.service;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.testapi.accounting.entity.AccountingEntry;
 import com.testapi.accounting.entity.User;
+import com.testapi.accounting.repository.AccountingEntryRepository;
 
 @Service
 public class AccountingEntryService {
@@ -18,45 +20,33 @@ public class AccountingEntryService {
         this.accountingEntryRepository = accountingEntryRepository;
     }
 
-    /**
-     * Finds an accounting entry by ID.
-     *
-     * @param id the ID of the accounting entry
-     * @return an Optional<AccountingEntry> which contains the entry if found
-     */
     public Optional<AccountingEntry> findById(Long id) {
         return accountingEntryRepository.findById(id);
     }
 
-    /**
-     * Retrieves all accounting entries from the database.
-     *
-     * @return a List of accounting entries
-     */
     public List<AccountingEntry> findAll(){
         return accountingEntryRepository.findAll();
     }
 
-    /**
-     * Saves a new accounting entry or updates an existing one.
-     *
-     * @param accountingEntry the entry to be saved
-     * @return the saved entry
-     */
     public AccountingEntry save(AccountingEntry accountingEntry){
         return accountingEntryRepository.save(accountingEntry);
     }
 
-    /**
-     * Deletes an accounting entry by ID.
-     *
-     * @param id the ID of the entry to be deleted
-     */
     public void deleteById(Long id){
         accountingEntryRepository.deleteById(id);
     }
 
     public List<AccountingEntry> findByUser(User user) {
         return accountingEntryRepository.findByOwner(user);
+    }
+
+    public ResponseEntity<AccountingEntry> updateEntry(Long id, AccountingEntry updatedEntry) {
+        return accountingEntryRepository.findById(id).map(entry -> {
+            entry.setEntryText(updatedEntry.getEntryText());
+            entry.setRecordType(updatedEntry.getRecordType());
+            entry.setAmount(updatedEntry.getAmount());
+
+            return ResponseEntity.ok(accountingEntryRepository.save(entry));
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
